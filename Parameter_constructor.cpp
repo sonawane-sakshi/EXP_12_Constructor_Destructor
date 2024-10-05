8f49cdecac59faf5eb45caf71f256585b4282f72
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-05 23:28:06 +0530

    Add files via upload

---
+++ Parameter_constructor.cpp
@@ -0,0 +1,29 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+
+class Pconstruct {
+    int area;
+    
+    public:
+    Pconstruct(int m, int n) {
+        area = m * n;
+    }
+
+    void display() {
+        cout << "Area: " << area << endl;
+    }
+};
+
+int main() {
+    int length, width;
+    
+    cout << "Enter length and width: ";
+    cin >> length >> width;
+
+    Pconstruct obj(length, width); 
+    obj.display(); 
+    
+    return 0;
+}
