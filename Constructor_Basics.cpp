7b42066fc69e7c1bb71629ad43558beda953e0b8
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-05 23:12:38 +0530

    Add files via upload

---
+++ Constructor_Basics.cpp
@@ -0,0 +1,18 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+class date{
+    int d,m,y;
+    public:
+    date()
+    {
+        cout<<"Constructor Created: "<<endl;
+        d=4;
+        m=9;
+        y=2024;
+    }
+};
+int main(){
+    date obj;
+}
