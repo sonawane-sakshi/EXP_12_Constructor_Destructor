4dfdbb8b6500fd68c661453408a803620e0ed7a5
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-06 10:20:10 +0530

    Add files via upload

---
+++ default_constructor.cpp
@@ -0,0 +1,17 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+class Construct {
+    public:
+    int a, b;
+    Construct() {
+        a = 10;
+        b = 20;
+    }
+};
+int main() {
+    Construct obj;
+    cout << "a: " << obj.a << ", b: " << obj.b << endl;
+    return 0;
+}
