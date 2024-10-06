00c37bf60169104fe3665d2449ba89c3be6a5695
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-06 11:18:24 +0530

    Add files via upload

---
+++ Method_Contructor.cpp
@@ -0,0 +1,28 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+class date {
+    int d, m, y;
+public:
+    date() {
+        cout << "Constructor called." << endl;
+    }
+    void inputDate() {
+        cout << "Enter date: ";
+        cin >> d;
+        cout << "Enter month: ";
+        cin >> m;
+        cout << "Enter year: ";
+        cin >> y;
+    }
+    void displayDate() {
+        cout << "The date is: " << d << "/" << m << "/" << y << endl;
+    }
+};
+int main() {
+    date obj;         
+    obj.inputDate();  
+    obj.displayDate(); 
+    return 0;
+}
